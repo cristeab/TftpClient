2b38977d377f0bb698c3f90b6c286216831b4af7
Bogdan Cristea <cristeab@gmail.com>
2019-08-15 01:08:59 -0700

    NSIS installer gen

--- build-win-release.bat
+++ build-win-release.bat
@@ -1,5 +1,4 @@
 set QT_PATH=E:/Qt/5.13.0/msvc2017_64
-set VC_REDIST_PATH=C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/VC/Redist/MSVC/14.16.27012
 
 rmdir /S /Q build
 
@@ -10,7 +9,7 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"  x64
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-"E:\Program Files\CMake\bin\cmake.exe" .. -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="%QT_PATH%"
+"E:\Program Files\CMake\bin\cmake.exe" .. -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="%QT_PATH%" -DPACKMSI=on
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 nmake
